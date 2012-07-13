6e1457b1e1daf72acade02dfa53f91692186a8c4
James Brewer <james@brewerhimself.com>
2012-07-13 16:13:49 -0400

    Added LICENSE.txt. Logr is covered under an MIT license.

--- logr.py
+++ logr.py
@@ -32,6 +32,7 @@ def index():
             files[file_] = []
             for f in listdir(os.path.join(ARTICLE_DIR, file_)):
                 if f.endswith('.md'):
+                    print os.path.join(ARTICLE_DIR, file_, f)
                     with open(os.path.join(ARTICLE_DIR, file_, '/', f), 'r') as f_open:
                         title=f_open.readline()
                         files[file_].append(dict(file_=f, slug=slugify(title), title=title.decode('utf-8')))
